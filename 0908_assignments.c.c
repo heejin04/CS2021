64fd2e945e3eb4446b42b856bc5161eece09c2b6
heejin04 <jinjinjin040404@gmail.com>
2021-09-15 13:50:01 +0900

    first commit

---
+++ 0908_assignments.c.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) {
+
+    printf("Somebody once said, \"Who's responsilbe for the riots?\n");
+    printf("The rioters.\"\n");
+    printf("'A' and \"A\" mean different things.\n");
+    printf("A backlash, \\, preceding a character is called\n");
+    printf("You must escape the ability of the ""to terminate\n");
+    printf("a string constant\n");
+    printf("by using \\", or the ability of the \\ to escapte, by using \\\\.\n");
+    printf("A stray \\, say \\z, in a string constant will have\n");
+    printf("different effects on differnet compilers.\n");
+    printf("\\n is a new line character.\n");
+    printf("\\r a carriage return\n");
+    printf("\\t a tab\n");
+    printf("\\a an alert\n");
+    printf("\\\\ a backlash\n");
+    printf("\\' a single quote\n");
+    printf("\\"" a double quote\n");
+    printf("\\n\\r\\t\\a\\\\\\\'\"\" is a string of escape sequence characters.\n");
+
+    return 0;
+}
